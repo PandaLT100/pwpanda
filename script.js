5b94d2ebbef17c0c34881851c28086d00c4a6651
PandaLT100 <162084864+PandaLT100@users.noreply.github.com>
2026-04-11 11:11:06 -0700

    i don't know what im doing anymore

--- script.js
+++ script.js
@@ -15,9 +15,7 @@ server.listen(port, hostname, () => {
 
 import { createRoot } from 'react-dom/client';
 
-// Clear the existing HTML content
-document.body.innerHTML = '<div id="app"></div>';
+/*The Code*/
 
-// Render your React component instead
-const root = createRoot(document.getElementById('app'));
-root.render(<h1>Hello, world</h1>);
+
+
