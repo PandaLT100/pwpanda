3fa6790afad4826605a67832c1dd058f629c9ac5
PandaLT100 <162084864+PandaLT100@users.noreply.github.com>
2026-04-18 12:15:55 -0700

    date stuff

--- dont.js
+++ dont.js
@@ -1,7 +1,23 @@
 function bobBurger() {
-    document.getElementById("mytext").innerHTML = "Ooga booga!";
-    document.getElementById("mytext").style.color = "red";
+    document.getElementById("hooray").innerHTML = "Correct!";
 } 
 
+function bobNoBurger() {
+    let xay = document.getElementById("hooray");
 
+    xay.innerHTML = "Incorrect!";
+}
 
+function goodDay() {
+    const hour = new Date().getHours();
+    let demo = document.getElementById("demo");
+    let greeting;
+    if (hour > 4) {
+        greeting = demo.innerHTML = "Good Morning";
+    } else if (4 < hour < 11) {
+        greeting = demo.innerHTML = "Good Morning, Again";
+    } else {
+        greeting = demo.innerHTML = "Good afternoon";
+    }
+
+}
